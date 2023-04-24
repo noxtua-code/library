1cd0fdd1dba49f733ead76d36efa40748eceb700
noxtua-code <93567294+noxtua-code@users.noreply.github.com>
2023-04-24 16:46:38 +1000

    Initial html and styling

--- library/main.js
+++ library/main.js
@@ -1,1 +1,25 @@
-import "./style.css";
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${
+    this.read ? "finished reading" : "not read yet"
+  }`;
+};
+
+function addBookToLibrary(newBookObject) {
+  myLibrary.push(newBookObject);
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary(theHobbit);
+
+console.log(theHobbit.info());
+console.log(Object.getPrototypeOf(theHobbit));
+console.log(myLibrary);
