677b657a34b01d8a26576402b8faf39e35983ed2
noxtua-code <93567294+noxtua-code@users.noreply.github.com>
2023-04-27 10:43:28 +1000

    Added functions to create new cards and append book details

--- library/main.js
+++ library/main.js
@@ -1,3 +1,4 @@
+const main = document.querySelector(".main");
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -17,9 +18,40 @@ function addBookToLibrary(newBookObject) {
   myLibrary.push(newBookObject);
 }
 
+// Add Book keys to list
+function appendBookDetails(newBookObject, bookList) {
+  Object.keys(newBookObject).forEach((key) => {
+    const bookDetail = document.createElement("li");
+
+    if (key === "pages") {
+      bookDetail.textContent = `${newBookObject[key]} pages`;
+    } else if (key === "read") {
+      if (newBookObject[key] === "true") {
+        bookDetail.textContent = `Finished reading`;
+      } else {
+        bookDetail.textContent = `Not read yet`;
+      }
+    } else {
+      bookDetail.textContent = newBookObject[key];
+    }
+
+    bookList.appendChild(bookDetail);
+  });
+}
+
+// Create Book card
+function createBookCard(newBookObject) {
+  const newCard = document.createElement("div");
+  newCard.className = "card";
+
+  const bookDetailsList = document.createElement("ul");
+  appendBookDetails(newBookObject, bookDetailsList);
+
+  newCard.appendChild(bookDetailsList);
+  main.appendChild(newCard);
+}
+
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 addBookToLibrary(theHobbit);
 
-console.log(theHobbit.info());
-console.log(Object.getPrototypeOf(theHobbit));
-console.log(myLibrary);
+createBookCard(theHobbit);
