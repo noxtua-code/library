eea8cf60852f8abdf59e8fb3f1e5e9d046304689
noxtua-code <93567294+noxtua-code@users.noreply.github.com>
2023-04-27 15:20:15 +1000

    Form functionality and new book submit button

--- library/main.js
+++ library/main.js
@@ -1,5 +1,11 @@
-const main = document.querySelector(".main");
+const main = document.querySelector("main");
 const myLibrary = [];
+const newBookButton = document.querySelector(".new-book-button");
+const newBookForm = document.querySelector("#new-book-form");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const read = document.querySelector("#read");
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -23,7 +29,7 @@ function appendBookDetails(newBookObject, bookList) {
   Object.keys(newBookObject).forEach((key) => {
     const bookDetail = document.createElement("li");
 
-    if (key === "pages") {
+    if (key === "pages" && pages.value !== "") {
       bookDetail.textContent = `${newBookObject[key]} pages`;
     } else if (key === "read") {
       if (newBookObject[key] === "true") {
@@ -49,9 +55,36 @@ function createBookCard(newBookObject) {
 
   newCard.appendChild(bookDetailsList);
   main.appendChild(newCard);
+  newCard.dataset.index = myLibrary.indexOf(newBookObject);
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
 addBookToLibrary(theHobbit);
 
-createBookCard(theHobbit);
+const gideonTheNinth = new Book("Gideon the Ninth", "Tamsyn Muir", 492, true);
+addBookToLibrary(gideonTheNinth);
+
+const bookHasSpiders = new Book(
+  "This book is full of spiders: seriously, dude, don't touch it",
+  "David Wong",
+  450,
+  false
+);
+addBookToLibrary(bookHasSpiders);
+
+Object.keys(myLibrary).forEach((item) => {
+  createBookCard(myLibrary[item]);
+});
+
+newBookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const newBook = new Book(title.value, author.value, pages.value, read.value);
+  addBookToLibrary(newBook);
+  createBookCard(myLibrary.at(-1));
+
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+  read.value = "";
+});
